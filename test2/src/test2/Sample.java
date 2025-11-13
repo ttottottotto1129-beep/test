3311d65ab0dc5be792c94c2a9c59ede5ee71a5e3
baehyenn <ttottottotto1129@gmail.com>
2025-11-13 23:09:47 +0900

    Second Upload입니다.

---
+++ test2/src/test2/Sample.java
@@ -0,0 +1,7 @@
+package test2;
+
+public class Sample {
+	public static void main(String[] args) {
+		System.out.println("과제입니다.");
+	}
+}
--- test2/src/test2/Test22.java
+++ test2/src/test2/Test22.java
@@ -3,5 +3,6 @@ package test2;
 public class Test22 {
 	public static void main(String[] args) {
 		System.out.println("Welcom GitHub!!");
+		System.out.println("Welcom GitHub!!222");
 	}
 }
